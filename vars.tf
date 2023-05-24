3bf6a049ce49cf52c9047b02e8a8a41cfafc4bdb
Anass <shaikanass304@gmail.com>
2023-05-24 21:08:50 +0530

    update terraform/

---
+++ vars.tf
@@ -0,0 +1,8 @@
+variable "env" {}
+variable "vpc_cidr" {}
+variable "tags" {}
+variable "public_subnets" {}
+variable "private_subnets" {}
+variable "default_vpc_id" {}
+variable "default_route_table" {}
+
